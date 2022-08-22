e2a253056f2fec411e1d32f82cbdc64a8066a8db
emmanuel-gyateng <57065066+Emmanuel-Okyere@users.noreply.github.com>
2022-08-22 12:47:52 +0000

    Changed the file structure tos suite the project

--- pyproject/core/config.py
+++
@@ -1,4 +0,0 @@
-"""Settings file for creating the prefix"""
-from fastapi import APIRouter
-
-router = APIRouter(prefix="/api/v1")
--- pyproject/main.py
+++ pyproject/main.py
@@ -1,42 +1,15 @@
 """Entry point the fast api"""
 
-from fastapi import FastAPI, status
+from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
-from transformers import T5ForConditionalGeneration, T5Tokenizer
-from .translate_service import translate_text
-from .core.config import router
-from .schemes.translate import Translations
+from .api.v1.translate import router
 
 
-app = FastAPI()
+app = FastAPI(title="Translate APP")
 app.include_router(router)
-
-
-tokenizer = T5Tokenizer.from_pretrained("t5-base")
-model = T5ForConditionalGeneration.from_pretrained("t5-base", return_dict=True)
 
 
 @app.get("/", include_in_schema=False)
 async def root():
     """Default root for the application where users get redirected to /doc"""
     return RedirectResponse(url="/docs")
-
-
-@app.post(
-    "/translate",
-    status_code=status.HTTP_200_OK,
-    response_description="Text Translated",
-)
-async def translate(request: Translations):
-    """Get the text from the user and translate to desired language"""
-    source_language = request.source_language
-    destination_language = request.destination_language
-    input_text = request.input_text
-    if source_language == destination_language:
-        return input_text
-    texts = translate_text(
-        model, tokenizer, source_language, destination_language, input_text
-    )
-    if texts == "":
-        return ""
-    return texts
